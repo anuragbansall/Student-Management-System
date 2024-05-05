5ff5d645a4df1804b9638702814c7aa7d95ad581
anuragbansal <anuragbits01@gmail.com>
2024-05-05 22:50:55 +0530

    minor changes

--- db_connection.php
+++ db_connection.php
@@ -1,13 +1,10 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = ""; 
+$password = ""; // php myadmin password 
 $database = "student_management";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $database);
-
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
